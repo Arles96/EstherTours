59c7e3e746b2284d26b70bc5fd6d57c96d8e1ba9
Arles96 <arles.cerrato@unitec.edu>
2018-11-28 14:07:04 -0600

    Se actualiza la información de la flota de arrendadoras

--- imports/startup/both/tabular/tabular.js
+++ imports/startup/both/tabular/tabular.js
@@ -2,6 +2,7 @@ import Tabular from 'meteor/aldeed:tabular';
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 import { Renters } from '../../../api/renters/renters';
+import { FleetRenter } from '../../../api/renters/fleetRenter';
 
 const TabularTables = {};
 
@@ -85,6 +86,45 @@ TabularTables.Renters = new Tabular.Table({
       class: 'text-center',
       createdCell: Meteor.isClient && function showButtonsRenters (cell, cellData, rowData) {
         return Blaze.renderWithData(Template.showButtonRenters, {
+          _id: rowData._id,
+          slug: rowData.slug
+        }, cell);
+      }
+    }
+  ]
+});
+
+TabularTables.FleetRenter = new Tabular.Table({
+  name: 'FleetRenter',
+  collection: FleetRenter,
+  responsive: true,
+  autoWidth: false,
+  search: {
+    caseInsesitive: true,
+    smart: true,
+    onEnterOnly: false
+  },
+  extraFields: ['menage', 'idRenter'],
+  columns: [
+    {
+      class: 'text-center',
+      data: 'total',
+      title: 'Total'
+    },
+    {
+      class: 'text-center',
+      data: 'type',
+      title: 'Tipo'
+    },
+    {
+      class: 'text-center',
+      data: 'rate',
+      title: 'Tarifa'
+    },
+    {
+      class: 'text-center',
+      createdCell: Meteor.isClient && function showButtonsFleetRenters (cell, cellData, rowData) {
+        return Blaze.renderWithData(Template.showButtonFleetRenters, {
           _id: rowData._id,
           slug: rowData.slug
         }, cell);
--- imports/ui/pages/renters/showInfoRenter.js
+++ imports/ui/pages/renters/showInfoRenter.js
@@ -1,1 +1,62 @@
 import './showInfoRenter.html';
+import '../../components/addFleetRenter/addFleetRenter';
+import './editFleetRenter';
+import toastr from 'toastr';
+import Swal from 'sweetalert2';
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+import { FleetRenter } from '../../../api/renters/fleetRenter';
+
+Template.showInfoRenter.onCreated(() => {
+  $.extend(true, $.fn.dataTable.defaults, {
+    language: {
+      sLengthMenu: 'Mostrar _MENU_ registros',
+      sProcessing: 'Procesando ...',
+      sZeroRecords: 'No se encontraron resultados',
+      sEmptyTable: 'Nigún dato disponible ',
+      sInfo: 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+      sInfoEmpty: 'Mostrando registros del 0 al 0 de un total de 0 registros',
+      sInfoFiltered: '(filtrado de un total de _MAX_ registros)',
+      sInfoPostFix: ' ',
+      sSearch: 'Buscar:',
+      sUrl: '',
+      sInfoThousands: ',',
+      sLoadingRecords: 'Cargando...',
+      oPaginate: {
+        sFirst: 'Primero',
+        sLast: 'Último',
+        sNext: 'Siguiente',
+        sPrevious: 'Anterior'
+      },
+      oAria: {
+        sSortAscending: ': Activar para ordenar la columna de manera ascendente',
+        sSortDescending: ': Activar para ordenar la columna de manera descendente'
+      }
+    }
+  });
+});
+
+Template.showButtonFleetRenters.events({
+  'click .deleteFleetRenter': function () {
+    const id = this._id;
+    Swal({
+      title: 'Eliminar Flota',
+      text: 'Esta seguro de eliminar este registro.',
+      cancelButtonText: 'Cancelar',
+      showCancelButton: true
+    }).then(res => {
+      if (res.value) {
+        Meteor.call('deleteFleetRenter', id, (error, result) => {
+          if (error) {
+            toastr.error('Error al eliminar el registro.');
+          } else {
+            toastr.success('Se eliminó el registro exitósamente.');
+          }
+        });
+      }
+    });
+  },
+  'click .editFleetRenter': function () {
+    Session.set('fleetRenter', FleetRenter.findOne({ _id: this._id }));
+  }
+});
