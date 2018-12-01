925af4f046e81cbc4b776cbd03104d02316337ba
agomezomg <agomez98@unitec.edu>
2018-11-30 18:03:32 -0600

    hello darkness my old friend

--- imports/ui/components/infoRestaurantOffer/infoRestaurantOffer.js
+++ imports/ui/components/infoRestaurantOffer/infoRestaurantOffer.js
@@ -3,7 +3,7 @@ import { Session } from 'meteor/session';
 
 Template.infoRestaurantOffer.helpers({
   restaurantOffer: () => {
-    console.log(Session.get('restaurantOffer'));
-    return Session.get('restaurantOffer');
+    console.log(Session.get('restaurantOffers'));
+    return Session.get('restaurantOffers');
   }
 });
