38c5219b6c2a1ecd4e578c8a478b8e6ba6dc267c
CJAlvarez <cristhianalvarez96@gmail.com>
2018-12-05 11:15:20 -0600

    Agregado: Listado de monedas importantes.

---
+++ imports/api/money/money.js
@@ -0,0 +1,88 @@
+const money = [
+  {
+    code: 'HNL',
+    symbol: 'L',
+    label: 'Lempira',
+    value: 'Lempira'
+  },
+  {
+    code: 'USD',
+    symbol: '$',
+    label: 'Dólar',
+    value: 'Dólar'
+  },
+  {
+    code: 'EUR',
+    symbol: '€',
+    label: 'Euro',
+    value: 'Euro'
+  },
+  {
+    code: 'CRC',
+    symbol: '₡',
+    label: 'Colón Costarricense',
+    value: 'Colón Costarricense'
+  },
+  {
+    code: 'NOK',
+    symbol: 'kr',
+    label: 'Corona Noruega',
+    value: 'Corona Noruega'
+  },
+  {
+    code: 'SEK',
+    symbol: 'kr',
+    label: 'Corona Sueca',
+    value: 'Corona Sueca'
+  },
+  {
+    code: 'NIO',
+    symbol: 'C$',
+    label: 'Córdoba Nicaragüense',
+    value: 'Córdoba Nicaragüense'
+  },
+  {
+    code: 'AUD',
+    symbol: '$',
+    label: 'Dólar Australiano',
+    value: 'Dólar Australiano'
+  },
+  {
+    code: 'BZD',
+    symbol: '$',
+    label: 'Dólar Beliceño',
+    value: 'Dólar Beliceño'
+  },
+  {
+    code: 'CAD',
+    symbol: '$',
+    label: 'Dólar Canadiense',
+    value: 'Dólar Canadiense'
+  },
+  {
+    code: 'HKD',
+    symbol: '$',
+    label: 'Dólar de Hong Kong',
+    value: 'Dólar de Hong Kong'
+  },
+  {
+    code: 'CHF',
+    symbol: 'Fr',
+    label: 'Franco Suizo',
+    value: 'Franco Suizo'
+  },
+  {
+    code: 'GBP',
+    symbol: '£',
+    label: 'Libra Esterlína',
+    value: 'Libra Esterlína'
+  },
+  {
+    code: 'GTQ',
+    symbol: 'Q',
+    label: 'Quetzal',
+    value: 'Quetzal'
+  }
+];
+
+export default money;
