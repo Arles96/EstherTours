3e788d8e8e55e5eb331c0311c12f2125bd6f8697
cano <canwoahzo@gmail.com>
2019-02-11 21:43:56 -0600

    Ajustes a interfaz de filtro

--- imports/ui/pages/attraction/filterAttractions.js
+++ imports/ui/pages/attraction/filterAttractions.js
@@ -1,10 +1,12 @@
 import './filterAttractions.html';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Attractions } from '../../../api/attractions/attractions';
-// import AttractionImages from '../../../api/attractions/attractionImage';
+import AttractionImages from '../../../api/attractions/attractionImage';
+
+// TODO mostrar por paginas
 
 Template.filterAttractions.onCreated(function createVars () {
-  this.precioMax = new ReactiveVar(100);
+  this.precioMax = new ReactiveVar(0);
 });
 
 Template.filterAttractions.helpers({
@@ -13,12 +15,27 @@ Template.filterAttractions.helpers({
   },
   buscar () {
     const precioMax = Template.instance().precioMax.get();
-    return Attractions.find({ price: { $lt: parseInt(precioMax, 10) } }).map(doc => doc);
+    return Attractions
+      .find({ price: { $lt: parseInt(precioMax, 10) } }, { sort: { price: 1 } })
+      .map(doc => doc);
   }
 });
 
 Template.filterAttractions.events({
   'change #sliderMax' (event, templateInstance) {
     templateInstance.precioMax.set(event.currentTarget.value);
+  }
+});
+
+Template.filterAttractions.onCreated(function createVars () {
+  this.precioMax = new ReactiveVar(0);
+});
+
+Template.filterResult.helpers({
+  findImg (_id) {
+    return AttractionImages.find({ _id }).map(doc => doc)[0];
+  },
+  first (index) {
+    return index === 0;
   }
 });
