8b38056ba5efb6258438bcd789ae417342ccb32a
Taufik1997 <taufikazzad@gmail.com>
2019-02-15 09:56:40 -0600

    bug

--- imports/api/branchOffices/method.js
+++ imports/api/branchOffices/method.js
@@ -5,6 +5,23 @@ import { admin } from '../roles/roles';
 Meteor.methods({
   insertOffice: function (doc) {
     if (Roles.userIsInRole(Meteor.userId(), admin)) {
+      const validate = branchOffices.findOne({
+        $or: [{
+          municipality: doc.idTransportationEstablishment,
+          street: doc.street,
+          city: doc.city,
+          departament: doc.departament,
+          phone: doc.phone
+        }, {
+          street: doc.street,
+          city: doc.city,
+          departament: doc.departament,
+          phone: doc.phone
+        }]
+      });
+      if (validate) {
+        throw new Meteor.Error('Ubicación duplicada.');
+      }
       branchOfficeSchema.validate(doc);
       branchOffices.insert(doc);
     } else {
@@ -15,6 +32,37 @@ Meteor.methods({
     if (Roles.userIsInRole(Meteor.userId(), admin)) {
       const data = doc.modifier.$set;
       const { _id } = doc;
+      const validate = branchOffices.find({
+        $or: [{
+          phone: data.phone,
+          street: data.street,
+          city: data.city,
+          department: data.department,
+          town: data.town
+        }, {
+          street: data.street,
+          city: data.city,
+          department: data.department,
+          town: data.town
+        }, {
+          street: data.street,
+          city: data.city,
+          department: data.department,
+          town: data.town
+        }, {
+          street: data.street,
+          city: data.city,
+          department: data.department,
+          town: data.town
+        }]
+      }).fetch();
+      if (validate.length > 0) {
+        validate.forEach(value => {
+          if (value._id !== doc._id) {
+            throw new Meteor.Error('Ubicación duplicada.');
+          }
+        });
+      }
       branchOfficeSchema.validate(data);
       branchOffices.update({ _id: _id }, {
         $set: data
