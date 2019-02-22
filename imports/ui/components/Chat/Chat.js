979b8f52152c2bac9b3c8970dbea6232f719b8bc
CJAlvarez <cristhianalvarez96@gmail.com>
2019-02-22 17:41:54 -0600

    Fix: Align Chats

--- imports/ui/components/Chat/Chat.js
+++ imports/ui/components/Chat/Chat.js
@@ -1,9 +1,18 @@
+import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
 import './Chat.html';
+
+Template.chat.onCreated(() => {
+});
 
 Template.chat.helpers({
   id: Meteor.userId()
 });
 
 Template.chat.events({
+  'click .closeChat': function (event) {
+    const openChats = Session.get('chatWith');
+    Session.set('chatWith', openChats.filter(item => item !== this.contextData.Issuer._id));
+  }
 });
--- imports/ui/components/ChatSide/ChatSide.js
+++ imports/ui/components/ChatSide/ChatSide.js
@@ -2,9 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import './ChatSide.html';
 import '../Chat/Chat';
 import '../ChatUserMenu/ChatUserMenu';
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+
+Template.chatSide.onCreated(() => {
+  Session.set('chatWith', []);
+});
 
 Template.chatSide.helpers({
-  openChats: null
+  openChats: () => {
+    const contextData = Session.get('chatWith').map(item => ({
+      Issuer: Meteor.users.findOne({ _id: item }),
+      Receiver: Meteor.user()
+    }));
+    return contextData;
+  }
 });
 
 Template.chatSide.events({
