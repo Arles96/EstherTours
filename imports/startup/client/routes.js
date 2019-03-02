9768f7168552d1dcdfffda67b3ea08d78a8ee09d
cano <canwoahzo@gmail.com>
2019-03-02 01:56:23 -0600

    Merge

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -85,22 +85,13 @@ import '../../ui/pages/RenterQuary/showRenters';
 import '../../ui/pages/findTransport/findTransport';
 import '../../ui/pages/resultTransport/resultTransport';
 import '../../ui/pages/branchOfficePage/officesPage';
-<<<<<<< HEAD
 import '../../ui/pages/packages/filterPackage';
 import '../../ui/pages/soldPackage/listSoldPackage';
 import '../../ui/pages/subscriptions/listSubscriptions';
 import '../../ui/pages/shoppingPackage/shoppingPackage';
-import { FleetRenter } from '../../api/renters/fleetRenter';
-import { RouteTransportationEstablishment } from '../../api/TransportationEstablishment/RouteTransportationEstablishment';
-import { RoomHotel } from '../../api/hotels/roomhotel';
-=======
 import '../../ui/pages/ChatPage/ChatPage';
-import '../../ui/pages/shoppingPackage/shoppingPackage';
 import '../../ui/pages/Activities/activities';
 import '../../ui/pages/Activities/activitiesFiltered';
-import '../../ui/pages/packages/filterPackage';
-import '../../ui/pages/soldPackage/listSoldPackage';
->>>>>>> 574de0b291cadc2ddeddd07245a4f9f53129605c
 
 /**
  *Función para listar en el componente breadcrumb
@@ -1715,7 +1706,6 @@ Router.route('/result-find-packages', {
 });
 
 /**
-<<<<<<< HEAD
  * Ruta para filtrar Paquetes
  */
 Router.route('/filter-packages', {
@@ -1756,7 +1746,10 @@ Router.route('/list-subscriptions', {
   onBeforeAction: function () {
     listBreadcrumb(['Tabla de Suscripciónes']);
     isConsultant(this);
-=======
+  }
+});
+
+/**
  * Ruta para página de chat
  */
 Router.route('/ChatPage', {
@@ -1772,7 +1765,6 @@ Router.route('/ChatPage', {
     Session.set('ShowChatFixed', false);
     listBreadcrumb(['Mensajes']);
     isLoggedIn(this);
->>>>>>> 574de0b291cadc2ddeddd07245a4f9f53129605c
   }
 });
 
@@ -1823,8 +1815,6 @@ Router.route('/adding-package', {
       renter,
       transportationEstablishment
     };
-<<<<<<< HEAD
-=======
   }
 });
 
@@ -1874,6 +1864,5 @@ Router.route('/sold-package', {
   onBeforeAction: function () {
     listBreadcrumb(['Tabla de Paquetes Vendidos']);
     isLoggedIn2(this);
->>>>>>> 574de0b291cadc2ddeddd07245a4f9f53129605c
   }
 });
