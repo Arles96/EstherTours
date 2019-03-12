3e795ab9b52bc9d0a4201b9f706f32747be89e5d
Arles96 <arles.cerrato@unitec.edu>
2019-03-11 22:47:27 -0600

    Se eliminado search bar

--- imports/api/tours/method.js
+++ imports/api/tours/method.js
@@ -1,9 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { Tours, ToursSchema } from './tours';
+import { operator } from '../roles/roles';
 
 Meteor.methods({
   addTours: function (doc) {
-    ToursSchema.validate(doc);
-    Tours.insert(doc);
+    if (Roles.userIsInRole(Meteor.userId(), operator)) {
+      ToursSchema.validate(doc);
+      Tours.insert(doc);
+    } else {
+      throw new Meteor.Error('Permiso Denegado');
+    }
+  },
+  editTours: function (doc) {
+    if (Roles.userIsInRole(Meteor.userId(), operator)) {
+      const data = doc.modifier.$set;
+      const { _id } = doc;
+      ToursSchema.validate(data);
+      Tours.update({ _id }, {
+        $set: data
+      });
+    } else {
+      throw new Meteor.Error('Permiso Denegado');
+    }
   }
 });
